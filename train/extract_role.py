1b94fd18d7cfbaafaffceeb71603ae77270ca5b4
XiongQIuQIu <zhangjinwei94@163.com>
2017-05-23 18:06:43 +0800

    some wrong

--- train/extract_role.py
+++ train/extract_role.py
@@ -26,6 +26,16 @@ def name_word_role(word, word_set):
         word_state = [[word[0], 'B'], [word[1], 'C'], [word[2], 'D']]
     return word_state
 
+
+def init(word, word_set):
+    word = word.split('/')
+    if word[1] != 'nr':
+        word[1] = 'A'
+    else:
+        word[1] = name_word_role(word, word_set)
+    return word
+
+
 def ex_role(line):
     word_set = load_word_data()
     line_list = line.split()
@@ -33,6 +43,11 @@ def ex_role(line):
     two_gram = []
     for i in range(len(line_list)-1):
         two_gram.append((line_list[i], line_list[i+1]))
+    if len(final) == 0:
+        try:
+            final.append(init(two_gram[0][0], word_set))
+        except:
+            print two_gram
     for i, word_two_gram in enumerate(two_gram):
         one_word_list = word_two_gram[0].split('/')
         two_word_list = word_two_gram[1].split('/')
@@ -60,20 +75,20 @@ def extract_2014():
     _write_path = os.path.normpath(os.path.join(os.getcwd(), os.path.dirname('2014_extract/')))
     dir_file = os.listdir(_data_path)
     for dir in dir_file:
-        write_file_path = os.path.normpath(os.path.join(_write_path, os.path.dirname(dir+'/')))
+        # write_file_path = os.path.normpath(os.path.join(_write_path, os.path.dirname(dir+'/')))
         dir_file_path = os.path.normpath(os.path.join(_data_path, os.path.dirname(dir+'/')))
         file_name_list = os.listdir(dir_file_path)
         for file in file_name_list:
             file_path = os.path.join(dir_file_path, file)
-            write_path = os.path.join(write_file_path, file)
+            write_path = os.path.join(_write_path, file)
             with open(file_path) as f:
                 for line in f:
-                    w_write_list = ex_role(line)
-                    print w_write_list
-                    write_line = ' '.join(i[0] + '/' + i[1] for i in w_write_list)
-                    f = open(write_path, 'w')
-                    f.write(write_line)
-                    f.close()
+                    if line.strp():
+                        w_write_list = ex_role(line.strip().decode('utf-8'))
+                        write_line = ' '.join(i[0] + '/' + i[1] for i in w_write_list)
+                        f = open(write_path, 'w')
+                        f.write(write_line.encode('utf-8'))
+                        f.close()
 
 if __name__ == '__main__':
     extract_2014()
